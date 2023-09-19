1ba7bf3419a116963c82e973a14852977f059020
shalanoveljan <eljanash@code.edu.az>
2023-09-19 11:24:24 +0400

    add

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ let btn=document.getElementById('btn');
 let monthly_payment=document.getElementById('monthly_payment')
 let general_payment=document.getElementById('general_payment')
 
+
+
+
 btn.onclick=() => {
 let kreditmebleg=parseInt(document.getElementById('Kredit_meblegi').value);
 let kreditmuddeti=parseInt(document.getElementById('Kredit_muddeti').value);
 let faiz=parseInt(document.getElementById('Faiz_derecesi').value);
-if(typeof kreditmebleg=="number" && typeof kreditmuddeti=="number" && typeof faiz=="number" ){
-general_payment=kreditmebleg+(kreditmebleg*(faiz/100));
-monthly_payment=(general_payment/kreditmuddeti)
-
-general_payment.innerHTML=`${general_payment}`
-monthly_payment.innerHTML=`${monthly_payment}`
-console.log(general_payment);
-console.log(monthly_payment);
-}
-else{
-    alert("Number daxil edin")
-}
-}
+    let General_payment=kreditmebleg+(kreditmebleg*(faiz/100));
+    let Monthly_payment=(General_payment/kreditmuddeti)
+    console.log( typeof kreditmebleg);  
+    general_payment.innerHTML=`${General_payment}`
+    monthly_payment.innerHTML=`${Monthly_payment}`
+    console.log(General_payment);
+    console.log(Monthly_payment);    
+} 
